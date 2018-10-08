680e562707f63140efb4a90a2b93436ed17fe01c
Andres Gonzalez <andres@MacBook-Pro-de-Andres-2.local>
2018-10-08 12:30:03 -0500

    webpack setup, hasta dev server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// console.log("hello");
+import React from "react";
+import ReactDOM from "react-dom"
+
+const Index = () => {
+  return <div>Hello react!!!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'));
